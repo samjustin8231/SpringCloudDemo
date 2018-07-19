179e16bdc01ffa30c7597f3f014a07e32e51a6bf
samjustin8231 <sam8231@163.com>
2018-07-19 17:10:12 +0800

    add service name for eureka server;

--- ServiceA/src/main/java/com/example/springcloud/servicea/ServiceApplication.java
+++ ServiceA/src/main/java/com/example/springcloud/servicea/ServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author sunyajun 2018/7/19 15:22
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @RestController
 public class ServiceApplication {
 
-    @Value("${spring.application.name}")
-    private String name;
-    @Value("${server.port}")
-    private String port;
-
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication.run( ServiceApplication.class, args );
     }
 
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping("/hi")
-    public String hi(@RequestParam String id) {
-        return "hi, " + id + ", " + name + ":" + port;
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
     }
 
 }
