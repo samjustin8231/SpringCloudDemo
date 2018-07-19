4232bd31be8b92bd2f7ce5a0652a77914254c625
samjustin8231 <sam8231@163.com>
2018-07-19 17:46:44 +0800

    add hystrix function for ribbon demo;

--- RibbonDemo/src/main/java/com/example/springcloud/ribbon/HelloService.java
+++ RibbonDemo/src/main/java/com/example/springcloud/ribbon/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.springcloud.ribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,9 +16,18 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    //创建了熔断器的功能，当访问的service-a服务挂了的时候，自动返回hiError的结果
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://service-a/hi?name=" + name, String.class);
     }
 
-
+    /**
+     * 发生熔断时，调用该方法
+     * @param name
+     * @return
+     */
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
 }
