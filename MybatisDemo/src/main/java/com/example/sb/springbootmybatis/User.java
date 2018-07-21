c15f2e5ce585a3f2b939baf9fb33c1bd2dcbc82f
samjustin8231 <sam8231@163.com>
2018-07-21 17:23:57 +0800

    add module MybatisDemo; use anotation to implement;

---
+++ MybatisDemo/src/main/java/com/example/sb/springbootmybatis/User.java
@@ -0,0 +1,60 @@
+package com.example.sb.springbootmybatis;
+
+import java.util.Objects;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String password;
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, password, phone);
+    }
+}
---
+++ MybatisDemo/src/test/java/com/example/sb/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.sb.springbootmybatis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMybatisApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void test(){
+
+        userMapper.insert("winterchen", "123456", "12345678910");
+        User u = userMapper.findUserByPhone("12345678910");
+        Assert.assertEquals("winterchen", u.getName());
+    }
+}
