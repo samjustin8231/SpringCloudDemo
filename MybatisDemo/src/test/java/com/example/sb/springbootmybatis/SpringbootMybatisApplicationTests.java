c0a71d1f285a7c99f4f1f9799378fe0be1432b15
samjustin8231 <sam8231@163.com>
2018-07-21 17:26:51 +0800

    test for transaction operation in MybatisDemo;

--- MybatisDemo/src/test/java/com/example/sb/springbootmybatis/SpringbootMybatisApplicationTests.java
+++ MybatisDemo/src/test/java/com/example/sb/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,9 +16,20 @@ public class SpringbootMybatisApplicationTests {
     private UserMapper userMapper;
 
     @Test
-    public void test(){
+    public void testInsertAndQuery(){
 
         userMapper.insert("winterchen", "123456", "12345678910");
+        User u = userMapper.findUserByPhone("12345678910");
+        Assert.assertEquals("winterchen", u.getName());
+    }
+
+    @Test
+    @Transactional
+    public void testTransaction(){
+
+        userMapper.insert("张三", "123456", "18600000000");
+        int a = 1/0;
+        userMapper.insert("李四", "123456", "13500000000");
         User u = userMapper.findUserByPhone("12345678910");
         Assert.assertEquals("winterchen", u.getName());
     }
